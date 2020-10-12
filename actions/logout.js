f4531901da8cd69b7912afa8dbc561291548e6a0
yushicheng <1542874601@qq.com>
2020-10-12 12:32:04 +0800

    init:项目初始化 星期一 下午 12:32:03 十月 12日 2020 项目初始化提交

---
+++ actions/logout.js
@@ -0,0 +1,11 @@
+const toast = require("@/utils/toast");
+const clearSecret = require("@/utils/secret/clearSecret");
+
+module.exports = async () => {
+  try {
+    await clearSecret();
+    toast.succeed("秘钥信息清除成功!");
+  } catch (error) {
+    throw error;
+  };
+};
